532a554f2d2df69913bb691a33a19ddc940e0dab
qujinshan <qujinshan@hujiang.com>
2016-07-28 16:44:50 +0800

    163爬虫完成

---
+++ music.py
@@ -0,0 +1,136 @@
+from pymongo import MongoClient
+from datetime import datetime
+import hashlib  
+import requests
+from util import session
+from util import log
+import os
+import traceback
+
+# client = MongoClient("mongodb://purkylin.com:27017")
+# db = client.dev
+# dogs = db.Dog
+# # print(dogs, dogs.count())
+# for o in dogs.find():
+# 	print(o['name'], o['age'])
+
+# now = datetime.datetime.utcnow()
+# dog = {"_created_at": now, '_updated_at': now, 'name': 'hisi', 'age':22}
+# myid = dogs.insert_one(dog).inserted_id
+# print(myid)
+
+client = MongoClient("mongodb://purkylin.com:27017")
+db = client.dev
+songs = db.Songs
+print('count', songs.count())
+
+# 榜单歌曲批量下载
+# 网易原创歌曲榜 云音乐飙升榜 云音乐热歌榜 云音乐新歌榜 itune
+list_id = ['2884035', '19723756', '3778678', '3778678', '11641012']
+header = {'Cookie': 'appver=1.5.0.75771', 'Referer': 'http://music.163.com/'}
+
+
+def downMusic(url, sid):
+	if os.path.exists('mp3/%s.mp3' % sid):
+		log.info('Already downloaded')
+		return
+
+	r = session.get(url)
+	with open('mp3/%s.mp3' % sid, 'wb') as fp:
+		fp.write(r.content)
+
+	log.info('Down finish')
+
+def isSongExists(sid):
+	if not sid:
+		print('Warn: sid is empty')
+		return True
+	song = songs.find_one({'sid': sid})
+
+	if song:
+		log.info(song['sid'])
+		return True
+	else:
+		return False
+
+
+class Song:
+	def __init__(self, sid, name, artist, url, createTime, source='163'):
+		self.sid = sid
+		self.name = name
+		self.artist = artist
+		self.url = url
+		self.createTime = createTime
+		self.source = source
+
+	def toJSON(self):
+		return {'sid': self.sid,
+				'name': self.name, 
+				'artist': self.artist,
+				'url': self.url, 
+				'createTime': self.createTime, 
+				'source': self.source
+		}
+
+	def isExists(self):
+		return songs.find({'sid': self.sid}) != None
+
+	def __str__(self):
+		return 'id:{}, name:{}'.format(self.sid, self.name)
+
+
+	def download(self):
+		downMusic(self.url, self.sid)
+
+	def save(self, needDownload=True):
+		if needDownload:
+			self.download()
+
+		d = self.toJSON()
+		iid = songs.insert_one(d).inserted_id
+		if iid:
+			log.info('insert success')
+		else:
+			log.error('insert fail')
+
+
+# http://music.163.com/api/song/detail/?id=28377211&ids=%5B28377211%5D
+
+
+def test163():
+	url = 'http://music.163.com/api/playlist/detail?id={}'.format(list_id[0])
+	log.info(url)
+	log.info('Grab album ...')
+	r = requests.get(url)
+	data = r.json()
+	result = data['result']
+	tracks = result['tracks']
+	total = len(tracks)
+
+	for i, item in enumerate(tracks):
+		log.info('Deal with {}th/{} song'.format(i + 1, total))
+		stamp = int(item['album']['publishTime']) / 1000
+		t = datetime.fromtimestamp(stamp)
+
+		if isSongExists(item['id']):
+			log.warn('The song exists and skip')
+			continue
+
+		song = Song(item['id'], item['name'], item['artists'][0]['name'], item['mp3Url'], t, '163')
+		song.save()
+
+
+def clearDB():
+	log.info('Clear database')
+	songs.delete_many({'sid': {"$gt": 0}})
+
+
+if __name__ == '__main__':
+	log.info('Begin')
+	# clearDB()
+	try:
+		test163()
+	except Exception as e:
+		log.info(traceback.format_exc(3)) 
+
+	log.info('Finish')
---
+++ util.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+
+import urllib
+# import http.cookiejar
+
+import shlex
+import requests
+import json
+import traceback
+import logging
+# from http.cookiejar import LWPCookieJar
+
+import smtplib
+from smtplib import SMTPAuthenticationError
+from email.mime.text import MIMEText
+
+ME = 'orighost@sina.com'
+USERNAME = 'orighost@sina.com'
+PASSWORD = 'xxxxxx'
+SMTP = 'smtp.sina.com'
+RECEIPT = ['qujinshan@hujiang.com', 'orighost@sina.com', 'liuxiaolong@hujiang.com']
+
+header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0"}
+session = requests.Session()
+session.headers.update(header)
+
+
+# create logger
+logger = logging.getLogger('music_163')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+fh = logging.FileHandler('163.log')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+log = logger
+
+
+def parseCurlCommand(s):
+	# 'curl -H "Host: www.renren.com" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:47.0) Gecko/20100101 Firefox/47.0" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" -H "Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3" -H "X-Requested-With: XMLHttpRequest" -H "Referer: http://www.renren.com/ajaxproxy.htm" -H "Cookie: anonymid=iqspb2qi-rp8hdc; depovince=GW; jebecookies=257e3ec3-9a35-405e-b697-0d56d6b96629|||||; _r01_=1; ick_login=87749530-8717-46c5-a0dc-bf2cebadd3fd; jebe_key=7b60a391-d2f5-4496-a464-57c54f55a8c7%7Ccfcd208495d565ef66e7dff9f98764da%7C1468886856874%7C0%7C1468886856894" --data "email=pengyouya123&icode=&origURL=http%3A%2F%2Fwww.renren.com%2Fhome&domain=renren.com&key_id=1&captcha_type=web_login&password=109233144d177ac10fe2412ea1e3a84136d25d2c33f9e205216827b31214abda&rkey=9801aa5c0a76cd5b3569ef42cbc8520b&f=" --compressed http://www.renren.com/ajaxLogin/login?1=1&uniqueTimestamp=201662834507'
+	lexer = shlex.shlex(s)
+	lexer.wordchars += '-'
+
+	tokens = []
+	parms = None
+	url = None
+
+	url = shlex.split(s)[-1]
+
+	while True:
+		t = lexer.read_token()
+		if not t:
+			break
+
+		if t == '-H':
+			tokens.append(lexer.get_token())
+		elif t == '--data-binary':  # may just --data
+			tmp = lexer.get_token().strip('"')
+			tmpData = urllib.parse.parse_qsl(tmp, keep_blank_values=True)
+			parms = {key: value for key, value in tmpData}
+
+	def hs(s):
+		x = s.strip('"')
+		t = x.split(': ')
+		return {t[0]: t[1]}
+
+	r = list(map(hs, tokens))
+	headers = {}
+
+	for item in r:
+		headers.update(item)
+
+	return (url, parms, headers)
+
+
+def formatCURL(cmd):
+	(url, parms, headers) = parseCurlCommand(cmd)
+	print(url)
+	print(parms)
+	print(headers)
+
+
+def simulateCURL(session, cmd):
+	(url, parms, headers) = parseCurlCommand(cmd)
+	# Cookie 自动传不需要设置
+	headers['Cookie'] = None
+	r = session.post(url, parms, headers=headers, verify=False)
+	# print(r.text)
+	return r
+
+
+# Read web page
+def readPage(url, timeout=10, json=False):
+	try:
+		r = session.get(url, timeout=10, verify=False)
+	except:
+		traceback.print_exc()
+		return None
+
+	if r.history:
+		print('Warn: Occur redirect')
+		return None
+
+	if json:
+		return r.json()
+	else:
+		return r.text
+
+
+# Format json string
+def formatJSONString(s):
+	data = json.loads(s)
+	result = json.dumps(data, sort_keys=True, indent=4)
+	return result
+
+
+def sendEmail(subject, body):
+	msg = MIMEText(body)
+	msg['Subject'] = subject
+	msg['To'] = ','.join(RECEIPT)
+	msg['From'] = ME
+
+	s = smtplib.SMTP(SMTP)
+	try:
+		s.login(USERNAME, PASSWORD)
+		s.sendmail(ME, RECEIPT, msg.as_string())
+	except SMTPAuthenticationError:
+		print('邮箱认证失败')
+	except Exception:
+		print('发送失败')
+	finally:
+		s.quit()
